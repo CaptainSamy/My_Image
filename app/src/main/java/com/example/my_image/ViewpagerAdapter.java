d415c8b54acbf2b60cf53b78c938eba2bc19cb75
CaptainSamy <thinhndph07008@gmail.com>
2020-07-29 22:49:06 +0700

    update

--- app/src/main/java/com/example/my_image/ViewpagerAdapter.java
+++ app/src/main/java/com/example/my_image/ViewpagerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.my_image;
 
 import android.content.Context;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
